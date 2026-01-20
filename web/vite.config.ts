959faf39fb7d2f7b9301742920554709707677ef
juliolmuller <juliolmuller@outlook.com>
2026-01-20 12:13:55 -0300

    chore: upgrade Vite and related to their latest versions

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,16 +1,7 @@
-import vue from '@vitejs/plugin-vue';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '~': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    port: 8080,
-  },
-});
+})
