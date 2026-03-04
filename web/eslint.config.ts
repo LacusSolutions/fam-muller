fd429c782127b6fc404e474fb41a1a88517ea4b0
juliolmuller <juliolmuller@outlook.com>
2026-03-04 10:41:50 -0300

    chore: fix linting

--- web/eslint.config.ts
+++ web/eslint.config.ts
@@ -4,8 +4,20 @@ import { defineConfig } from 'eslint/config';
 export default defineConfig([
   ...anyConfig.vue,
   {
+    files: ['**/*.vue', '**/*.ts', '**/*.tsx', '**/*.js', '**/*.mjs', '**/*.cjs', '**/*.jsx'],
     rules: {
       'import/namespace': 'off',
+    },
+    settings: {
+      'import/ignore': ['^@phosphor-icons/vue$'],
+    },
+  },
+  {
+    files: ['**/StatsSection/StatsSection.vue'],
+    rules: {
+      'import/named': 'off',
+      'import/default': 'off',
+      'import/export': 'off',
     },
   },
 ]);
